a8e7abd3500df5b6349fba5ec1e9760cb20cc25b
Michele <lottomichele98@gmail.com>
2024-12-30 03:27:03 +0100

    scripts

--- train_net.sh
+++ train_net.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-#SBATCH --job-name=prepare_data
+#SBATCH --job-name=train_net
 #SBATCH --time=1-00:00:00 # days-hh:mm:ss
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1
-#SBATCH -o prepare_dataset.log
+#SBATCH --gpus-per-task=1
+#SBATCH -o train_net.log
 
 #importante: crea il tuo env conda (lo puoi fare con i comandi standard)
 #poi attivalo in questo modo:
@@ -12,4 +13,4 @@ eval "$(conda shell.bash hook)"
 conda activate DL4NLP
 
 #esegui il tuo codice qua
-python3 dataset_creation_AMI.py --split all
+python3 train_net.py --num-gpus 1 --config-file configs/diffdet.ami.ast.yaml
