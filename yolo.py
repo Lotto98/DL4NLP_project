dae45a46d72fbbd952f2c41f400e06b9f6a048c4
Michele <lottomichele98@gmail.com>
2025-01-18 15:48:14 +0100

    yolo training start
    Co-authored-by: Davide Tonetto <davide.tonetto2001@gmail.com>

--- yolo.py
+++ yolo.py
@@ -6,17 +6,18 @@ model = YOLO("yolo11n.pt")
 # Train the model
 train_results = model.train(
     data="datasets/ami_yolo/ami.yaml",  # path to dataset YAML
-    epochs=1,  # number of training epochs
+    epochs=10,  # number of training epochs
     device="0",  # device to run on, i.e. device=0 or device=0,1,2,3 or device=cpu
-    batch=8,  # batch size
+    batch=16,  # batch size
+    #imgsz=640,  # image size
 )
 
 # Evaluate model performance on the validation set
 metrics = model.val()
 
 # Perform object detection on an image
-results = model("path/to/image.jpg")
-results[0].show()
+#results = model("path/to/image.jpg")
+#results[0].show()
 
 # Export the model to ONNX format
 path = model.export(format="onnx")  # return path to exported model
