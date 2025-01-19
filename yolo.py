877182ab69c8e4a6652d0a5cbbe6abfc5cafc0b5
Lotto98 <875922@stude.unive.it>
2025-01-19 17:22:30 +0100

    training yolo medium too

--- yolo.py
+++ yolo.py
@@ -1,13 +1,24 @@
 from ultralytics import YOLO
+import argparse
+
+# Parse command line arguments
+parser = argparse.ArgumentParser()
+parser.add_argument("--model", type=str, help="model.pt path", required=True, 
+                    choices=["yolo11n.pt", "yolo11s.pt", "yolo11m.pt", "yolo11l.pt", "yolo11x.pt"])
+parser.add_argument("--device", type=int, help="cuda device, i.e. 0 or 0,1,2,3 or cpu", required=True)
+parser.add_argument("--epochs", type=int, help="number of training epochs", required=True)
+
+
+args = parser.parse_args()
 
 # Load a model
-model = YOLO("yolo11n.pt")
+model = YOLO(args.model)
 
 # Train the model
 train_results = model.train(
     data="datasets/ami_yolo/ami.yaml",  # path to dataset YAML
-    epochs=500,  # number of training epochs
-    device="1",  # device to run on, i.e. device=0 or device=0,1,2,3 or device=cpu
+    epochs=args.epochs,  # number of training epochs
+    device=args.device,  # device to run on, i.e. device=0 or device=0,1,2,3 or device=cpu
     batch=0.80,  # batch size
     plots=True,  # create plots
 )
